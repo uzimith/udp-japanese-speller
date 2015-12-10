71de0558e88de0d12273b75b9090771031316cad
uzimith <uzimith.x9@gmail.com>
2015-12-11 00:20:59 +0900

    add Devtools

--- src/client.tsx
+++ src/client.tsx
@@ -3,7 +3,9 @@ import * as ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import App from './containers/App';
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 import configureStore from './stores/configureStore';
+
 // const ipcRenderer = require('electron').ipcRenderer;
 //
 // ipcRenderer.on('receive-command', (event, arg) => {
@@ -12,9 +14,21 @@ import configureStore from './stores/configureStore';
 
 const store = configureStore();
 
+let debug;
+if (process.env.NODE_ENV !== 'production') {
+  debug = (
+    <DebugPanel top right bottom>
+      <DevTools store={store} monitor={LogMonitor} />
+    </DebugPanel>
+  );
+}
+
 ReactDOM.render(
-        <Provider store={store}>
-            <App />
-        </Provider>,
+        <div>
+          <Provider store={store}>
+              <App />
+          </Provider>
+          {debug}
+        </div>,
         document.getElementById('root')
         );
